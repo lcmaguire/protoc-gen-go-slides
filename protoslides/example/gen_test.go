a789379fa9611c427b6ebed3d9a41de7c17be322
Liam <79071192+LiamMaguireANZ@users.noreply.github.com>
2024-06-04 15:37:46 +1000

    Keep state

--- protoslides/example/gen_test.go
+++ protoslides/example/gen_test.go
@@ -1,6 +1,7 @@
 package example
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/lcmaguire/protoc-gen-go-slides/example"
@@ -24,4 +25,24 @@ func TestXxx(t *testing.T) {
 		Text: "text",
 	}
 
+}
+
+type PageSizer interface {
+	GetPageSize() int32
+}
+
+func HandlePageSize(in PageSizer) (int32, error) {
+	if in.GetPageSize() < 0 {
+		return -1, errors.New("invalid cannot be less than 0")
+	}
+
+	if in.GetPageSize() > 1000 {
+		return 1000, nil
+	}
+
+	if in.GetPageSize() == 0 {
+		return 25, nil
+	}
+
+	return in.GetPageSize(), nil
 }
